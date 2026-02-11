702fe7c81d31ccc502dbc972fa3e0017099290fd
Fargetik <davidonsky1234@gmail.com>
2026-02-11 17:57:10 +0200

    Добавили небольшие проверки корректности данных

--- www/index.php
+++ www/index.php
@@ -1,5 +1,15 @@
 <?php session_start(); ?>
 
+<?php if(isset($_SESSION['errors'])): ?>
+    <ul style="color:red;">
+        <?php foreach($_SESSION['errors'] as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php unset($_SESSION['errors']); ?>
+<?php endif; ?>
+
+
 <?php if(isset($_SESSION['username'])): ?>
     <p>Данные из сессии:</p>
     <ul>
